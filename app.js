6b18e78d28b00eb352f19854ad13dad5b7a8fdb7
AleksandrVrachenko <vrachenkoaleksandr@gmail.com>
2022-10-27 21:03:32 +0300

    HW #11

--- app.js
+++ app.js
@@ -4,26 +4,10 @@ function sumDigits(number) {
         number =- number;
     }
     let sum = 0;
-    let res = 0;
-    for(let i = 0;number > 0;i++) {
-        res = number % 10;
-        sum += res;
-        number /= 10;
+    while(number > 0){
+        sum += number % 10;
+        number = Math.floor(number/10);
     }
     return sum;
 }
 console.log(sumDigits(-2222));
-
-function sumDigits1(number) {
-    if(number < 0){
-        number =- number;
-    }
-    let res = String(number);
-    let sum = 0;
-
-    for(let i = 0; i < res.length; i++) {
-         sum += Number(res[i]);
-    }
-    return sum;
-}
-console.log(sumDigits1(-2222));
