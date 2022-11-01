b8943f89beb746e4f569514dc88cecb942a02a9d
AleksandrVrachenko <vrachenkoaleksandr@gmail.com>
2022-11-01 15:40:25 +0200

    HW #12

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 
-function sumDigits(number) {
-    if(number < 0) {
-        number =- number;
-    }
-    let sum = 0;
-    while(number > 0){
-        sum += number % 10;
-        number = Math.floor(number/10);
-    }
-    return sum;
+function fromStringToNumber(string, base) {
+    
+             let result = 0;
+             for (let i = 0; i < string.length; i++) {
+
+                let code = string.charCodeAt(i) - 55;
+                result = result * base + code;
+             }
+             return result;
+        
 }
-console.log(sumDigits(-2222));
+console.log(fromStringToNumber("JAVA", 36));
+
+
+
+function fromNumberToString(number, base) {
+    number = Math.abs(number);
+    let res = "";
+    do {
+
+        let digit = number % base;
+
+        if (base == 36){
+            let sym = String.fromCharCode(digit + 55);
+            res = sym + res;
+        }else{
+            let sym = digit == 0 ? '0' : '1';
+            res = sym + res;
+        }
+        
+        number = Math.trunc(number / base);
+
+     } while(number != 0);
+    
+     return res;
+ 
+}
+console.log(fromNumberToString(900550, 36));
