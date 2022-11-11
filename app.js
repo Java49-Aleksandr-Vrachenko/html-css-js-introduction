b7c36e2513e7b6e87b7923b7943e3eee60cf03fe
AleksandrVrachenko <vrachenkoaleksandr@gmail.com>
2022-11-11 23:23:22 +0200

    HW #15

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
 console.log("/*********************Task 1********************/");
-let string = ["abc","lmn","cd",];
-let newString = console.log(ulSurround(string));
-function ulSurround(string) {
-   
-    let newString = string.map(function(elm,ind){
-       return "<li>" + elm + "<li>";
-    })
-    newString.unshift("<ul>");
-    newString.push("/ul");
-    return newString;
+let numbers = [1, 2, 3, 4, 5];
+
+function minMax(numbers) {
+      return numbers.reduce((previousValue, currentValue) => {
+            if(currentValue < previousValue[0]) {
+                  previousValue[0] = currentValue;
+            }
+            if(currentValue > previousValue[1]) {
+                  previousValue[1] = currentValue;
+            }
+            return previousValue;
+      
+      }, [numbers[0], numbers[0]]);
 }
+console.log(`Input: [${numbers}], Output: [${minMax(numbers)}]`)
+
 
 console.log("/*********************Task 2********************/");
 
-let strings = ["abc", "lmn","cd","abc","abc"];
-let check = "abc";
-console.log(count(strings, check));
+let strings = ["abc", "old_abc", "lmn", "123", "old_lmn"];
+let prefix = "old_";
 
-function count(strings, check){
-    let count = 0;
-    strings.forEach(function(e) {
-        if(check == e)
-           count++;
-});
-    return count;
-};
+function deleteWithPrefix(strings, prefix) {
+   return strings.filter(s => !s.startsWith(prefix));  
+}
+console.log(`Input: [${strings}], Prefix: ${prefix}, Output: [${deleteWithPrefix(strings,prefix)}]`);
 
 console.log("/*********************Task 3********************/");
 
-let arS = [1,2,3,4,5,6,7];
-let arD = [10,20,30,40,50,60,70];
-arrayCopy(arS, 3, arD, 4, 3);
-console.log("arD=",arD);
+let number = [1,6,3,8,5,2,7,4];
 
-function arrayCopy(src, srcPos, dst, dstPos, length) {
-    let cpy = src.slice(srcPos, srcPos+length);
-    dst.splice(dstPos, 0, ...src.slice(srcPos, srcPos+length));
-};
-
-console.log("/*********************Task 4********************/");
-
-let number = [1, 2, 3, 4, 5, 6, 7];
-move(number, 2, 4);
-console.log(number);
-
-function move(number, index, position) {
-    number.splice(index + position, 0, number.splice(index,1)[0]);
+function getSortedEvenOdd(number) {
+    return number.slice().sort((a, b) => {
+            return a % 2 == 0 && a < b ? -1 : b % 2 == 0 ? 1 : b - a;
+      })
 }
+console.log(`Input: [${number}], Output: [${getSortedEvenOdd(number)}]`);
